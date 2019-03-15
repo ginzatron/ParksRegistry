394bff2d4a09dca97e13884df5d64bbaeabc0f67
Noah Ginsburg <njginsburg@gmail.com>
2019-03-15 11:24:41 -0400

    survey tests

---
+++ WebApplication.Tests/SurveySqlDAOTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApplication.Tests.DAL;
+using WebApplication.Web.DAL;
+using WebApplication.Web.Models;
+
+namespace WebApplication.Tests
+{
+    [TestClass]
+    public class SurveySqlDAOTests : CapstoneTests
+    {
+
+        [TestMethod]
+        public void GetAllSurveysReturnsOneSurvey()
+        {
+            SurveySqlDAO surveyDAO = new SurveySqlDAO(ConnectionString);
+            IList<SurveyViewModel> surveys = SurveySqlDAO.GetAllSurveys();
+
+            Assert.AreEqual(1, surveys.Count);
+        }
+    }
+}
